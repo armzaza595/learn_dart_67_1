42a373e1b5a9e1984bad371e440877ba4a14a7e0
Tiwat <armlnw.58@gmail.com>
2025-07-26 14:10:00 +0700

    Step 1: Structure

---
+++ dart.dart
@@ -0,0 +1,11 @@
+void main(){
+    var name ="John";
+    print(name);
+    print("Your name is $name");
+    print("Your name is $name Jane");
+
+    int age = 28;
+    print("Your age is $age,");
+
+    String userName = "Alice";
+}
