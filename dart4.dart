b1bd542b4635a63152fcb49006a858f2f85cdba4
Tiwat <armlnw.58@gmail.com>
2025-07-26 15:18:51 +0700

    Step5

---
+++ dart4.dart
@@ -0,0 +1,8 @@
+void main(){
+  dynamic dyamicVariable = 42 ;
+  print(dyamicVariable);
+   dyamicVariable = "Hello, Dart" ;
+    print(dyamicVariable);
+    dyamicVariable = "1, 2, 3" ;
+    print(dyamicVariable);
+} 
